6f35a5efa9e214b668e9b717f256a3b4036c4d42
lenny <lennyrappro@gmail.com>
2025-05-19 17:07:39 +0200

    🆕 Add vue Name

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import MyCounter from './components/MyCounter.vue'
+import MyName from "@/components/MyName.vue";
+
 </script>
 
 <template>
   <MyCounter />
+  <MyName />
 </template>
 
 <style>
---
+++ src/components/MyName.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+</script>
+
+<template>
+  <div class="name-display">
+    <h1>Entrez votre prénom :</h1>
+    <input v-model="name" type="text" placeholder="Votre prénom" />
+
+    <p v-if="name">Bonjour, {{ name }} !</p>
+    <p v-else>Veuillez entrer votre prénom</p>
+  </div>
+</template>
+
+<style scoped>
+.name-display {
+  text-align: center;
+  margin-top: 50px;
+  font-family: sans-serif;
+}
+
+input {
+  padding: 8px 12px;
+  font-size: 16px;
+}
+</style>
